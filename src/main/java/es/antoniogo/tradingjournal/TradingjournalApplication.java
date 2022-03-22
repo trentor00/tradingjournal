9518920d6e217539fd1239c750416f665f7225c9
Antonio González <trentor00@gmail.com>
2022-03-22 18:23:46 +0100

    Initial commit

---
+++ src/main/java/es/antoniogo/tradingjournal/TradingjournalApplication.java
@@ -0,0 +1,13 @@
+package es.antoniogo.tradingjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingjournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingjournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/antoniogo/tradingjournal/TradingjournalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.antoniogo.tradingjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingjournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
